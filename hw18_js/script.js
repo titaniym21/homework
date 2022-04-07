dfc037f2a5efdce0581bc2381120e83fc7d100e2
titaniym21 <lanko.oleksandr@gmail.com>
2022-04-07 17:11:45 +0300

    hw18_js_work

--- hw18_js/script.js
+++ hw18_js/script.js
@@ -1,1 +1,19 @@
+//task1
+document.querySelector('.btn-task1').addEventListener('click', function() {
+    var a = Number(document.querySelector('.num1-task1').value);
+    var b = Number(document.querySelector('.num2-task1').value);
+    if (a > b) {
+        let buff = a;
+        a = b;
+        b = buff;
+    }
+    var c = a;
+    while(a < b) {
+        a++;
+        c += a;
+    }
+    document.querySelector('.outnum-task1').innerHTML = c ;
+});
+
+//task2
 
