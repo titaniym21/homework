ebecd96bf24cf58f3721b32dea19deae636da044
titaniym21 <lanko.oleksandr@gmail.com>
2022-04-08 17:33:51 +0300

    hw18_js_work

--- hw18_js/script.js
+++ hw18_js/script.js
@@ -32,20 +32,20 @@ document.querySelector(".btn-task2").addEventListener("click", function () {
 //task3
 document.querySelector(".btn-task3").addEventListener("click", function () {
   var a = Number(document.querySelector(".num1-task3").value);
-  var arr = [] ,
+  var arr = [],
     i = 0;
   if (a == 0) {
-    document.querySelector(".outnum-task3").innerHTML ="У ноля нет делителей!";
+    document.querySelector(".outnum-task3").innerHTML = "У ноля нет делителей!";
     return;
   } else if (a < 0) {
-    document.querySelector(".outnum-task3").innerHTML ="Число должно быть положительным!";
+    document.querySelector(".outnum-task3").innerHTML =
+      "Число должно быть положительным!";
     return;
-  }
-  else {
-  while (i++ <= a) {
-        if (a % i == 0) {
-            arr.push(i);
-        }
+  } else {
+    while (i++ <= a) {
+      if (a % i == 0) {
+        arr.push(i);
+      }
     }
   }
   document.querySelector(".outnum-task3").innerHTML = arr;
@@ -56,3 +56,28 @@ document.querySelector(".btn-task4").addEventListener("click", function () {
   var a = String(document.querySelector(".num1-task4").value);
   document.querySelector(".outnum-task4").innerHTML = a.length;
 });
+
+//task5
+var arr = [];
+document.querySelector(".btn1-task5").addEventListener("click", function () {
+  arr.push(Number(document.querySelector(".num1-task5").value));
+  document.querySelector(".outnum-task5").innerHTML = arr;
+});
+var arrpos = [];
+var arrneg = [];
+var arrzero = [];
+document.querySelector(".btn2-task5").addEventListener("click", function (){
+  for(var item in arr){
+    if(arr[item]>0){
+      arrpos.push(arr[item]);
+    }else if(arr[item]<0){
+      arrneg.push(arr[item]);
+    }else{
+      arrzero.push(arr[item]);
+    }
+  }
+  document.querySelector(".posnum-task5").innerHTML = "Чисел положительных : " + arrpos.length;
+  document.querySelector(".negnum-task5").innerHTML = "Чисел отрицательных : " + arrneg.length;
+  document.querySelector(".zeronum-task5").innerHTML ="Чисел равных нулю : " + arrzero.length ;
+});
+
