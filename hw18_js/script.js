d670b52179bb419607e5a7f418840042d1503ff6
titaniym21 <lanko.oleksandr@gmail.com>
2022-04-12 00:10:55 +0300

    hw18_js

--- hw18_js/script.js
+++ hw18_js/script.js
@@ -66,18 +66,91 @@ document.querySelector(".btn1-task5").addEventListener("click", function () {
 var arrpos = [];
 var arrneg = [];
 var arrzero = [];
-document.querySelector(".btn2-task5").addEventListener("click", function (){
-  for(var item in arr){
-    if(arr[item]>0){
+document.querySelector(".btn2-task5").addEventListener("click", function () {
+  for (var item in arr) {
+    if (arr[item] > 0) {
       arrpos.push(arr[item]);
-    }else if(arr[item]<0){
+    } else if (arr[item] < 0) {
       arrneg.push(arr[item]);
-    }else{
+    } else {
       arrzero.push(arr[item]);
     }
   }
-  document.querySelector(".posnum-task5").innerHTML = "Чисел положительных : " + arrpos.length;
-  document.querySelector(".negnum-task5").innerHTML = "Чисел отрицательных : " + arrneg.length;
-  document.querySelector(".zeronum-task5").innerHTML ="Чисел равных нулю : " + arrzero.length ;
+  document.querySelector(".posnum-task5").innerHTML =
+    "Чисел положительных : " + arrpos.length;
+  document.querySelector(".negnum-task5").innerHTML =
+    "Чисел отрицательных : " + arrneg.length;
+  document.querySelector(".zeronum-task5").innerHTML =
+    "Чисел равных нулю : " + arrzero.length;
 });
 
+//task6
+function task_6() {
+  while (true) {
+    var a = prompt("Введите первое число");
+    var b = prompt("Введите второе число");
+    var x = prompt("Введите знак");
+    switch (x) {
+      case "+":
+        alert(Number(a) + Number(b));
+        break;
+      case "-":
+        alert(Number(a) - Number(b));
+        break;
+      case "*":
+        alert(Number(a) * Number(b));
+        break;
+      case "/":
+        alert(Number(a) / Number(b));
+        break;
+      default:
+        alert("Вы ввели неверный знак");
+    }
+    var y = prompt("Хотите продолжить? Введите да или нет");
+    if (y == "нет") {
+      break;
+    }
+  }
+}
+
+//task7
+document.querySelector(".btn-task7").addEventListener("click", function () {
+  var a = Number(document.querySelector(".num1-task7").value);
+  var b = Number(document.querySelector(".num2-task7").value);
+  if (String(a).length > b) {
+    
+  } 
+  else if(String(a).length == b) {
+    document.querySelector(".outnum-task7").innerHTML = a;
+  }
+  else {
+    document.querySelector(".outnum-task7").innerHTML =
+      "Нет такого количества знаков в числе!";
+  }
+});
+//task8
+function task_8() {
+  const days = [
+    "понедельник",
+    "вторник",
+    "среда",
+    "четверг",
+    "пятница",
+    "суббота",
+    "воскресенье",
+  ];
+  var currDay = 0;
+  while (confirm(`${days[currDay]}. Хотите увидеть следующий день?`)) {
+    currDay = (currDay + 1) % days.length;
+  }
+}
+//task9
+document.querySelector(".btn-task9").addEventListener("click", function () {
+  for (var i = 2; i < 10; i++) {
+    for (var j = 1; j <= 10; j++) {
+      document.querySelector(".outnum-task9").innerHTML +=
+        "<div>" + i + "*" + j + "=" + i * j + "</div>";
+    }
+  }
+  document.querySelector(".btn-task9").style.display = "none";
+});
