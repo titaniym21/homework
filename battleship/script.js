2a7078afa697dbfc545b3a562ed6be64d48184a5
titaniym21 <lanko.oleksandr@gmail.com>
2022-04-28 22:55:34 +0300

    battleship 28.04.2022

--- battleship/script.js
+++ battleship/script.js
@@ -5,45 +5,75 @@ document.querySelector(".button").addEventListener("click", function () {
     tmp = tmp.toUpperCase();
     let coordinat;
     if ((tmp[0] < 65 || tmp[0] > 72 || tmp[1] < 49 || tmp[1] > 56) && tmp.length == 2) {
-      coordinat = tmp;
-      document.querySelector(".message").innerHTML = "OK";
+        coordinat = tmp;
+        document.querySelector(".message").innerHTML = "OK";
     } else {
-      document.querySelector(".message").innerHTML =
-        "Введите корректные координаты";
+        document.querySelector(".message").innerHTML =
+            "Введите корректные координаты";
     }
     checkShip(arrShip, coordinat);
 });
 
 
 function radomShip(arr) {
-  let tmp;
-  for (let i = 0; i < 3; i++) {
-    let x = Math.floor(Math.random() * (3 - 1) + 1);
-        if (x == 1) {
-            let char1 = String.fromCharCode(Math.floor(Math.random() * 7) + 65);
-            let char2 = Math.floor(Math.random() * 7) ;
-            tmp = char1 + char2;
-        } else {
-            let char1 = String.fromCharCode(Math.floor(Math.random() * 7) + 65);
-            let char2 = Math.floor(Math.random() * 7) ;
-            tmp = char1 + char2;
-        }
-    arr.push(tmp);
-  }
-  return arr;
+    let tmp;
+    for (let i = 0; i < 3; i++) {
+        let x = Math.floor(Math.random() * 2);
+        let length = 3;
+        let char1 = Math.floor(Math.random() * 7);
+        let char2 = Math.floor(Math.random() * 7);
+        tmp = String.fromCharCode(char1 + 65) + char2;
+            if(arr.indexOf(tmp) == -1){
+                if(x == 1){
+                    arr.push(tmp);
+                    let chartest = char2;
+                    length--;
+                    while (length > 0) {
+                        if (chartest < 6) {
+                            char2++;
+                        } else {
+                            char2--;
+                        }
+                        tmp = String.fromCharCode(char1 + 65) + char2;
+                        arr.push(tmp);
+                        length--;
+                    }
+                } else {
+                    tmp = String.fromCharCode(char1 + 65) + char2;
+                    let chartest = char1;
+                    arr.push(tmp);
+                    length--;
+                    while (length > 0) {
+                        if (chartest < 6) {
+                            char1++;
+                        } else {
+                            char1--;
+                        }
+                        tmp = String.fromCharCode(char1 + 65) + char2;
+                        arr.push(tmp);
+                        length--;
+                    }
+                }
+            }else{
+                i--;
+            }
+    }
+    return arr;
 }
+
+
 radomShip(arrShip);
 console.log(arrShip);
 
 
 
 function checkShip(arr, data) {
-  let tmp = arr.indexOf(data);
-  if (tmp == -1) {
-    document.querySelector("." + data).classList.add("miss");
-  } else {
-    document.querySelector("." + data).classList.add("hit");
-  }
+    let tmp = arr.indexOf(data);
+    if (tmp == -1) {
+        document.querySelector("." + data).classList.add("miss");
+    } else {
+        document.querySelector("." + data).classList.add("hit");
+    }
 }
 
 
