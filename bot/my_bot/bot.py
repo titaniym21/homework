b81caa8d8ed01614364ff4ac25869d0e4a8b6bd3
titaniym21 <72406204+titaniym21@users.noreply.github.com>
2022-06-13 21:08:44 +0300

    delete_bot

--- bot/my_bot/bot.py
+++
@@ -1,9 +0,0 @@
-from aiogram import executor
-from dispatcher import dp
-import personal_actions
-
-from db import BotDB
-BotDB = BotDB('db.db')
-
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
